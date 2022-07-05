2de1974685f01cc41f85b3063ce6b5c5ded2fba3
Devarora09876 <107344517+Devarora09876@users.noreply.github.com>
2022-07-06 00:36:49 +0530

    Update couple.py

--- FallenRobot/modules/couple.py
+++ FallenRobot/modules/couple.py
@@ -50,8 +50,8 @@ async def couple(_, message):
             c1_mention = (await pbot.get_users(c1_id)).mention
             c2_mention = (await pbot.get_users(c2_id)).mention
 
-            couple_selection_message = f"""**Couple of the day:**
-{c1_mention} + {c2_mention} = 😘
+            couple_selection_message = f"""**Couple of the day🥀:**
+🥀 {c1_mention} 🥀 + 🥀 {c2_mention} 🥀 = 💖
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
             await pbot.send_message(message.chat.id, text=couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
@@ -62,8 +62,8 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
             c2_id = int(is_selected["c2_id"])
             c1_name = (await pbot.get_users(c1_id)).first_name
             c2_name = (await pbot.get_users(c2_id)).first_name
-            couple_selection_message = f"""Couple of the day:
-[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = 😘
+            couple_selection_message = f"""Couple of the day🥀:
+[{c1_name}](tg://openmessage?user_id={c1_id}) 🥀 + [{c2_name}](tg://openmessage?user_id={c2_id}) 🥀 = 💖
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
             await pbot.send_message(message.chat.id, text=couple_selection_message)
     except Exception as e:
