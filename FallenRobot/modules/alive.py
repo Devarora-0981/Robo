a4291ea0a20c3c1e24bd1123ea0feedb5db36a35
Devarora09876 <107344517+Devarora09876@users.noreply.github.com>
2022-07-07 17:25:31 +0530

    Add files via upload

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -1,50 +1,60 @@
-import os
-import re
-import random
-from asyncio import sleep as lvda
-from platform import python_version as kontol
-from telethon import events, Button
-from telegram import __version__ as telever
-from telethon import __version__ as tlhver
-from pyrogram import __version__ as pyrover
-from FallenRobot.events import register
-from FallenRobot import telethn as tbot
+#Don't Remove This Line 
+#TG : @Abishnoi1M 
+#Channel : @Abishnoi_bots
+#Github : KingAbishnoi 
 
+
+# add yours... 
+
+import random
+import asyncio
+from pyrogram import filters, __version__ as pver
+from sys import version_info
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from telethon import __version__ as tver
+from telegram import __version__ as lver
+from AbishnoiRobot import BOT_USERNAME, OWNER_USERNAME, SUPPORT_CHAT, pbot
 
 PHOTO = [
-    "https://telegra.ph/file/26aec6d41ffd788f624d9.jpg",
-    "https://telegra.ph/file/26aec6d41ffd788f624d9.jpg",
+    "https://telegra.ph/file/47f1c6b57321808e9eb61.jpg",
+    "https://telegra.ph/file/d2433e011fb8eff1650f8.mp4",
+    "https://telegra.ph/file/4af05a90d3058915d20e6.jpg",
+    "https://telegra.ph/file/a0a79755bc3336f47a30b.jpg",
+    "https://telegra.ph/file/c35acfb3cd4699c7a9e2c.jpg",
 ]
 
+BYABISHNOI = [
+    [
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇꜱ", url=f"https://t.me/Abishnoi_bots"),
+        InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
+    ],
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url=f"https://t.me/Abishnoi_ro_bot?startgroup=true"),
+    ],
+]
 
-@register(pattern=("/alive"))
-async def awake(event):
-    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ ᴅᴇᴠ ✘ ʀᴏʙᴏᴛ​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝐃𝐄𝐕](https://t.me/devarora0981)** \n\n"
-    TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
-    TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
-    TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
-    BUTTON = [
-        [
-            Button.url("ʜᴇʟᴘ​", "https://t.me/Dev_managerbot?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/we_rfriends"),
-        ]
-    ]
-    ran = random.choice(PHOTO)
-    l = await event.reply("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ...")
-    await lvda(1)
-    o = await l.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ.........")
-    await lvda(1)
-    d = await o.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ...")
-    await lvda(1)
-    a = await d.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ.........")
-    await lvda(1)
-    await a.delete()
-    # x = await tbot.send_file(event.chat_id, file="CAACAgUAAx0CWOSA3AABBtPDYsX2Vt5j5vHwrarbQLgTX1kO-LQAAvAIAAJicH0p17vHbwZpufkpBA")
-    # await lvda(1.5)
-    # await x.delete()
-    await tbot.send_file(event.chat_id, ran, caption=TEXT, buttons=BUTTON)
-
-
-## Alive mod
-# By Hyper
+@pbot.on_message(filters.command("alive"))
+async def restart(client, m: Message):
+    await m.delete()
+    accha = await m.reply("⚡")
+    await asyncio.sleep(1)
+    await accha.edit("ᴀʟɪᴠɪɴɢ..")
+    await asyncio.sleep(0.1)
+    await accha.edit("ᴀʟɪᴠɪɴɢ...")
+    await accha.delete()
+    await asyncio.sleep(0.1)
+    umm = await m.reply_sticker("CAACAgUAAx0CUgguZAACdL9iuTsITX6x0Z7kSMhZ_2IeIBlmewAC8gUAAnEhyFVGFPeLco2P_x4E")
+    await umm.delete()
+    await asyncio.sleep(0.1)
+    await m.reply_photo(
+        random.choice(PHOTO),
+        caption=f"""**ʜᴇʏ, ɪ ᴀᴍ ᴀʙɢ 𖠌 ʀᴏʙᴏᴛ**
+        ━━━━━━━━━━━━━━━━━━━
+  » **ᴍʏ ᴏᴡɴᴇʀ :** [𝗔𝗕𝗜𝗦𝗛𝗡𝗢𝗜](https://t.me/{OWNER_USERNAME})
+  » **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{lver}`
+  » **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tver}`
+  » **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pver}`
+  » **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{version_info[0]}.{version_info[1]}.{version_info[2]}`
+        ━━━━━━━━━━━━━━━━━━━""",
+        reply_markup=InlineKeyboardMarkup(BYABISHNOI)
+    )
