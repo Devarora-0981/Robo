3c455a66ac236af586b652286877d231f5b251e6
Devarora09876 <107344517+Devarora09876@users.noreply.github.com>
2022-07-11 21:27:31 +0530

    Update karma.py

--- FallenRobot/modules/karma.py
+++ FallenRobot/modules/karma.py
@@ -27,6 +27,7 @@ regex_downvote = r"^(\-|\-\-|\-1|👎|💔|noob|weak)$"
 karma_positive_group = 3
 karma_negative_group = 4
 
+
 @app.on_message(
     filters.text
     & filters.group
@@ -40,6 +41,40 @@ karma_negative_group = 4
 @capture_err
 async def upvote(_, message):
     if not await is_karma_on(message.chat.id):
+        return
+    if not message.reply_to_message.from_user:
+        return
+    if not message.from_user:
+        return
+    if message.reply_to_message.from_user.id == message.from_user.id:
+        return
+    chat_id = message.chat.id
+    user_id = message.reply_to_message.from_user.id
+    user_mention = message.reply_to_message.from_user.mention
+    current_karma = await get_karma(chat_id, await int_to_alpha(user_id))
+    if current_karma:
+        current_karma = current_karma["karma"]
+        karma = current_karma + 1
+    else:
+        karma = 1
+    new_karma = {"karma": karma}
+    await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
+    await message.reply_text(
+        f"Incremented Karma of {user_mention} By 1 \nTotal Points: {karma}"
+    )
+@app.on_message(
+    filters.text
+    & filters.group
+    & filters.incoming
+    & filters.reply
+    & filters.regex(regex_upvote)
+    & ~filters.via_bot
+    & ~filters.bot,
+    group=karma_positive_group,
+)
+@capture_err
+async def upvote(_, message):
+    if not is_karma_on(message.chat.id):
         return
     if not message.reply_to_message.from_user:
         return
