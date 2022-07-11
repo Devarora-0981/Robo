418db59b86211b0efdfbcd0fbf8f322fed835323
Devarora09876 <107344517+Devarora09876@users.noreply.github.com>
2022-07-11 21:25:45 +0530

    Update and rename Karma.py to karma.py

---
+++ FallenRobot/modules/karma.py
@@ -0,0 +1,65 @@
+import asyncio
+from pyrogram import filters
+from aiohttp import ClientSession
+from Python_ARQ import ARQ
+
+from FallenRobot import pbot as app
+from FallenRobot.utils.errors import capture_err
+from FallenRobot.utils.permissions import adminsOnly
+from FallenRobot.helper_extra.dbfun import (
+    alpha_to_int,
+    get_karma,
+    get_karmas,
+    int_to_alpha,
+    is_karma_on,
+    karma_off,
+    karma_on,
+    update_karma,
+)
+from FallenRobot import arq
+
+regex_upvote = (
+    r"^((?i)\+|\+\+|\+1|thx|thanx|thanks|thankyou|love|pro|🖤|❣️|💝|💖|💕|❤|💘|cool|good|👍)$"
+)
+regex_downvote = r"^(\-|\-\-|\-1|👎|💔|noob|weak)$"
+
+
+karma_positive_group = 3
+karma_negative_group = 4
+
+@app.on_message(
+    filters.text
+    & filters.group
+    & filters.incoming
+    & filters.reply
+    & filters.regex(regex_upvote)
+    & ~filters.via_bot
+    & ~filters.bot,
+    group=karma_positive_group,
+)
+@capture_err
+async def upvote(_, message):
+    if not await is_karma_on(message.chat.id):
+        return
+    if not message.reply_to_message.from_user:
+        return
+    if not message.from_user:
+        return
+    if message.reply_to_message.from_user.id == message.from_user.id:
+        return
+    chat_id = message.chat.id
+    user_id = message.reply_to_message.from_user.id
+    user_mention = message.reply_to_message.from_user.mention
+    current_karma = await get_karma(chat_id, await int_to_alpha(user_id))
+    if current_karma:
+        current_karma = current_karma["karma"]
+        karma = current_karma + 1
+    else:
+        karma = 1
+    new_karma = {"karma": karma}
+    await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
+    await message.reply_text(
+        f"Incremented Karma of {user_mention} By 1 \nTotal Points: {karma}"
+    )
+
+
