0153be2d106d9cd95116556dcb854dd272bceb6c
Devarora09876 <107344517+Devarora09876@users.noreply.github.com>
2022-07-15 10:40:31 +0530

    Update devs.py

--- FallenRobot/modules/devs.py
+++ FallenRobot/modules/devs.py
@@ -550,5 +550,137 @@ def devlist(update: Update, context: CallbackContext):
     m.edit_text(reply, parse_mode=ParseMode.HTML)
 
 
+__help__ = f"""
+*⚠️ Notice:*
+Commands listed here only work for users with special access are mainly used for troubleshooting, debugging purposes.
+Group admins/group owners do not need these commands. 
+*List all special users:*
+ ❍ /dragons*:* Lists all Dragon disasters
+ ❍ /demons*:* Lists all Demon disasters
+ ❍ /tigers*:* Lists all Tigers disasters
+ ❍ /wolves*:* Lists all Wolf disasters
+ ❍ /heroes*:* Lists all Hero Association members
+ ❍ /adddragon*:* Adds a user to Dragon
+ ❍ /adddemon*:* Adds a user to Demon
+ ❍ /addtiger*:* Adds a user to Tiger
+ ❍ /addwolf*:* Adds a user to Wolf
+ ❍ `Add dev doesnt exist, devs should know how to add themselves`
+*Ping:*
+ ❍ /ping*:* gets ping time of bot to telegram server
+ ❍ /pingall*:* gets all listed ping times
+*Broadcast: (Bot owner only)*
+*Note:* This supports basic markdown
+ ❍ /broadcastall*:* Broadcasts everywhere
+ ❍ /broadcastusers*:* Broadcasts too all users
+ ❍ /broadcastgroups*:* Broadcasts too all groups
+*Groups Info:*
+ ❍ /groups*:* List the groups with Name, ID, members count as a txt
+ ❍ /leave <ID>*:* Leave the group, ID must have hyphen
+ ❍ /stats*:* Shows overall bot stats
+ ❍ /getchats*:* Gets a list of group names the user has been seen in. Bot owner only
+ ❍ /ginfo username/link/ID*:* Pulls info panel for entire group
+*Access control:* 
+ ❍ /ignore*:* Blacklists a user from using the bot entirely
+ ❍ /lockdown <off/on>*:* Toggles bot adding to groups
+ ❍ /notice*:* Removes user from blacklist
+ ❍ /ignoredlist*:* Lists ignored users
+*Speedtest:*
+ ❍ /speedtest*:* Runs a speedtest and gives you 2 options to choose from, text or image output
+*Module loading:*
+ ❍ /listmodules*:* Lists names of all modules
+ ❍ /load modulename*:* Loads the said module to memory without restarting.
+ ❍ /unload modulename*:* Loads the said module frommemory without restarting memory without restarting the bot 
+*Remote commands:*
+ ❍ /rban*:* user group*:* Remote ban
+ ❍ /runban*:* user group*:* Remote un-ban
+ ❍ /rpunch*:* user group*:* Remote punch
+ ❍ /rmute*:* user group*:* Remote mute
+ ❍ /runmute*:* user group*:* Remote un-mute
+*Windows self hosted only:*
+ ❍ /reboot*:* Restarts the bots service
+ ❍ /gitpull*:* Pulls the repo and then restarts the bots service
+*Chatbot:* 
+ ❍ /listaichats*:* Lists the chats the chatmode is enabled in
+ 
+*Debugging and Shell:* 
+ ❍ /debug <on/off>*:* Logs commands to updates.txt
+ ❍ /logs*:* Run this in support group to get logs in pm
+ ❍ /eval*:* Self explanatory
+ ❍ /sh*:* Runs shell command
+ ❍ /shell*:* Runs shell command
+ ❍ /clearlocals*:* As the name goes
+ ❍ /dbcleanup*:* Removes deleted accs and groups from db
+ ❍ /py*:* Runs python code
+ 
+*Global Bans:*
+ ❍ /gban <id> <reason>*:* Gbans the user, works by reply too
+ ❍ /ungban*:* Ungbans the user, same usage as gban
+ ❍ /gbanlist*:* Outputs a list of gbanned users
+*Global Blue Text*
+ ❍ /gignoreblue*:* <word>*:* Globally ignore bluetext cleaning of saved word across Anonymous Robot.
+ ❍ /ungignoreblue*:* <word>*:* Remove said command from global cleaning list
+*Masha Core*
+*Owner only*
+ ❍ /send*:* <module name>*:* Send module
+ ❍ /install*:* <reply to a .py>*:* Install module 
+*Heroku Settings*
+*Owner only*
+ ❍ /usage*:* Check your heroku dyno hours remaining.
+ ❍ /see var <var>*:* Get your existing varibles, use it only on your private group!
+ ❍ /set var <newvar> <vavariable>*:* Add new variable or update existing value variable.
+ ❍ /del var <var>*:* Delete existing variable.
+ ❍ /logs Get heroku dyno logs.
+`⚠️ Read from top`
+Visit @{SUPPORT_CHAT} for more information.
+"""
+
+SUDO_HANDLER = CommandHandler(("addsudo", "adddragon"), addsudo)
+SUPPORT_HANDLER = CommandHandler(("addsupport", "adddemon"), addsupport)
+TIGER_HANDLER = CommandHandler(("addtiger"), addtiger)
+WHITELIST_HANDLER = CommandHandler(("addwhitelist", "addwolf"), addwhitelist)
+UNSUDO_HANDLER = CommandHandler(("removesudo", "removedragon"), removesudo)
+UNSUPPORT_HANDLER = CommandHandler(("removesupport", "removedemon"), removesupport)
+UNTIGER_HANDLER = CommandHandler(("removetiger"), removetiger)
+UNWHITELIST_HANDLER = CommandHandler(("removewhitelist", "removewolf"), removewhitelist)
+
+WHITELISTLIST_HANDLER = CommandHandler(["whitelistlist", "wolves"], whitelistlist)
+TIGERLIST_HANDLER = CommandHandler(["tigers"], tigerlist)
+SUPPORTLIST_HANDLER = CommandHandler(["supportlist", "demons"], supportlist)
+SUDOLIST_HANDLER = CommandHandler(["sudolist", "dragons"], sudolist)
+DEVLIST_HANDLER = CommandHandler(["devlist", "heroes"], devlist)
+
+dispatcher.add_handler(SUDO_HANDLER)
+dispatcher.add_handler(SUPPORT_HANDLER)
+dispatcher.add_handler(TIGER_HANDLER)
+dispatcher.add_handler(WHITELIST_HANDLER)
+dispatcher.add_handler(UNSUDO_HANDLER)
+dispatcher.add_handler(UNSUPPORT_HANDLER)
+dispatcher.add_handler(UNTIGER_HANDLER)
+dispatcher.add_handler(UNWHITELIST_HANDLER)
+
+dispatcher.add_handler(WHITELISTLIST_HANDLER)
+dispatcher.add_handler(TIGERLIST_HANDLER)
+dispatcher.add_handler(SUPPORTLIST_HANDLER)
+dispatcher.add_handler(SUDOLIST_HANDLER)
+dispatcher.add_handler(DEVLIST_HANDLER)
+
+__mod_name__ = "Devs​"
+__handlers__ = [
+    SUDO_HANDLER,
+    SUPPORT_HANDLER,
+    TIGER_HANDLER,
+    WHITELIST_HANDLER,
+    UNSUDO_HANDLER,
+    UNSUPPORT_HANDLER,
+    UNTIGER_HANDLER,
+    UNWHITELIST_HANDLER,
+    WHITELISTLIST_HANDLER,
+    TIGERLIST_HANDLER,
+    SUPPORTLIST_HANDLER,
+    SUDOLIST_HANDLER,
+    DEVLIST_HANDLER,
+]
+
+
 
 
