4e407ff5645d4a90c5ad1286bf589bf2bce3e776
cyberxron <99018732+cyberxron@users.noreply.github.com>
2022-07-15 10:49:52 +0530

    Update disasters.py

--- FallenRobot/modules/disasters.py
+++ FallenRobot/modules/disasters.py
@@ -678,7 +678,6 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Devs​"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
