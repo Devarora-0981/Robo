0c42e608d879ab326d522114da7a84e45c9a38ac
Dev Arora <107177633+Devarora0981@users.noreply.github.com>
2022-07-27 00:49:00 +0530

    Update musicals.py

--- FallenRobot/modules/musicals.py
+++ FallenRobot/modules/musicals.py
@@ -3,4 +3,6 @@ __mod_name__ = "Mᴜsɪᴄ"
 __help__ = """
  ❍ /song <song name>*:* Uploads the song in it's best quality available
  💡Ex: `/song Faded Alan Walker`
+ ❍ /mplay <song name>*:* Play The Music in the best quality.
+ ❍ /mhelp *:* For Music Bot Help Command 
 """
