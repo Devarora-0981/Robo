d2448f202961b12c724717ee7dbf987dcc3bd3d4
Dev Arora <107177633+Devarora0981@users.noreply.github.com>
2022-07-27 00:38:15 +0530

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -7,13 +7,13 @@ from pyrogram import filters
 from FallenRobot import pbot as client
 
 
-DEV = "https://telegra.ph/file/26aec6d41ffd788f624d9.jpg"
+GANDU_BSDK = "https://telegra.ph/file/21ba91a40cf7cdac1c881.mp4"
 
 
 @client.on_message(filters.command(["repo", "source", "gand"]))
 async def repo(client, message):
     await message.reply_animation(
-        animation=DEV,
+        animation=GANDU_BSDK,
         caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [ᴅᴇᴠ ✘ ʀᴏʙᴏᴛ-🇮🇳](t.me/dev_managerbot)**
 
 **» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [𝐃𝐄𝐕](t.me/devarora0981)
