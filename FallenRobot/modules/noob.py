e2086727cec37b0084b784e1b2ae1fba152668b4
Dev Arora <107177633+Devarora0981@users.noreply.github.com>
2022-07-26 23:07:09 +0530

    Update noob.py

--- FallenRobot/modules/noob.py
+++ FallenRobot/modules/noob.py
@@ -22,7 +22,7 @@ ABISHNOI = "https://telegra.ph/file/0820acefbb082356a19cb.mp4"
 
 @bot.on_message(filters.command(["noob", "owner", "chutiya"]))
 async def repo(client, message):
-    await message.reply_photo(
+    await message.reply_animation(
         photo=ABISHNOI,  # don't change
         caption=f"""**ʜᴇʏ {message.from_user.mention()},\n\nɪ ᴀᴍ [『𝘿𝙀𝙑 ✘ 𝙍𝙊𝘽𝙊𝙏』](t.me/dev_managerbot)**
 """,
