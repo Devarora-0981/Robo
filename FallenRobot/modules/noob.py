cd5f84c4e0c644ae6f2384c1614248bf617ccc77
Dev Arora <107177633+Devarora0981@users.noreply.github.com>
2022-07-30 08:45:45 +0530

    Update noob.py

--- FallenRobot/modules/noob.py
+++ FallenRobot/modules/noob.py
@@ -20,7 +20,7 @@ from FallenRobot import pbot as bot
 ABISHNOI = "https://telegra.ph/file/0820acefbb082356a19cb.mp4"
 
 
-@bot.on_message(filters.command(["noob", "owner", "chutiya"]))
+@bot.on_message(filters.command(["noob", "owner", "chutiya", "gandu", "bsdk", "lvde", "madarchod", "bkl", "lund"]))
 async def repo(client, message):
     await message.reply_animation(
         animation=ABISHNOI,  # don't change
