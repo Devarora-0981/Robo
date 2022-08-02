8a6fc3860b35d82142bef4dcdc2cce48be218775
Dev Arora <107177633+Devarora0981@users.noreply.github.com>
2022-08-03 01:51:17 +0530

    Update indian.py

--- FallenRobot/modules/indian.py
+++ FallenRobot/modules/indian.py
@@ -6,5 +6,13 @@ from FallenRobot import pbot as FallenRobot
 
 
 
-INDIAN_STRINGS = 
-                [
+INDIAN_STRINGS = [
+                  "No matter what our religion, in the end, we are all Indians. May our nation become the most prosperous in the world. Happy Independence Day!",
+                  "Today I breathe the air of freedom because of the efforts of our great freedom fighters. Happy Independence Day!",
+                  "May the Indian tricolour always fly high. Sending you warm wishes on the grand occasion of Independence Day!",
+                  "Let’s salute the martyrs For the sacrifices they made, And thank them For giving us our today.",
+                  "Today is a day to feel proud about being a part of this great nation. May this spirit of freedom leads us all to success and glory in life. Happy Independence Day!",
+                  "We may never know how it feels like to live in a free country if it was not for the bravery of our fathers. Today they deserve a big salute from us. Happy Independence Day!",
+                 ]
+
+
