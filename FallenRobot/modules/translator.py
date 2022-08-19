651270c6e0cc9fb656b0cb55f65773a4a534ff15
stdrosebot <104678019+stdrosebot@users.noreply.github.com>
2022-08-19 18:24:56 +0530

    Update translator.py

--- FallenRobot/modules/translator.py
+++ FallenRobot/modules/translator.py
@@ -15,7 +15,7 @@ def totranslate(update: Update, context: CallbackContext) -> None:
             "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
             "Example: `/tr en-hi` to translate from English to Hindi\n"
             "Or use: `/tr en` for automatic detection and translating it into english.\n"
-            "Click here to see [List of available Language Codes](https://t.me/DevilsHeavenMF/148391).",
+            "Click here to see [List of available Language Codes](https://t.me/Mahakal_Support/17).",
             parse_mode="markdown",
             disable_web_page_preview=True,
         )
