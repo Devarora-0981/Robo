9d79e6512f7a2b0d66a7ef91fc2186d5e898e2cc
stdrosebot <104678019+stdrosebot@users.noreply.github.com>
2022-08-19 18:11:48 +0530

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -7,30 +7,30 @@ from pyrogram import filters
 from FallenRobot import pbot as client
 
 
-GANDU_BSDK = "https://telegra.ph/file/21ba91a40cf7cdac1c881.mp4"
+GANDU_BSDK = "https://telegra.ph/file/51582c9510773813eba8e.jpg"
 
 
 @client.on_message(filters.command(["repo", "source", "gand"]))
 async def repo(client, message):
     await message.reply_animation(
         animation=GANDU_BSDK,
-        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [ᴅᴇᴠ ✘ ʀᴏʙᴏᴛ-🇮🇳](t.me/dev_managerbot)**
+        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [ᴍᴀʜᴀᴋᴀʟ ʀᴏʙᴏᴛ-🇮🇳](t.me/MahakalRobot)**
 
-**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [𝐃𝐄𝐕](t.me/devarora0981)
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [𝐒𝐓𝐃](t.me/STD_KING)
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
 **» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
 
-**ᴅᴇᴠ ✘ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ᴘʀɪᴠᴀᴛᴇ 🥺 sᴏ ᴜsᴇ ᴅᴇᴠ ʀᴏʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘs.**
+**ᴍᴀʜᴀᴋᴀʟ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ᴘʀɪᴠᴀᴛᴇ 🥺 sᴏ ᴜsᴇ ᴍᴀʜᴀᴋᴀʟ ʀᴏʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘs.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="t.me/devarora0981"),
+                    InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="t.me/STD_KING"),
                     InlineKeyboardButton(
                         "• sᴜᴘᴘᴏʀᴛ •",
-                        url="https://t.me/we_rfriends",
+                        url="https://t.me/Mahakal_Support",
                     ),
                 ]
             ]
