a46bb2d6ea386fa29bfda813d934d317bb64b992
stdrosebot <104678019+stdrosebot@users.noreply.github.com>
2022-08-23 16:27:12 +0530

    Update writetool.py

--- FallenRobot/modules/writetool.py
+++ FallenRobot/modules/writetool.py
@@ -17,7 +17,7 @@ async def handwriting(_, message):
     await m.edit("» ᴜᴩʟᴏᴀᴅɪɴɢ...")
     await pbot.send_chat_action(message.chat.id, "upload_photo")
     await message.reply_photo(
-        hand, caption="ᴡʀɪᴛᴛᴇɴ ᴡɪᴛʜ 🖊 ʙʏ [ᴍᴀʜᴀᴋᴀʟ ʀᴏʙᴏᴛ](t.me/MahakalRobot)"
+        hand, caption="ᴡʀɪᴛᴛᴇɴ ᴡɪᴛʜ 🖊 ʙʏ [ꜱʜɪᴋʜᴀ ʀᴏʙᴏᴛ](t.me/Shikha_Robot)"
     )
 
 
